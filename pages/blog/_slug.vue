8833f6fabe5b97ab621f0829e881580ce5fcf449
Théophile Desmedt <theophiledesmedt@gmail.com>
2023-11-10 00:07:16 +0100

    add each page view

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,28 @@
+<template>
+    <main class="post individual" v-if="post">
+      <h1>{{ post.title.rendered }}</h1>
+      <section v-html="post.content.rendered"></section>
+    </main>
+    <div v-else>Loading...</div>
+  </template>
+    
+    <script>
+    export default {
+    computed: {
+      posts() {
+        return this.$store.state.posts;
+      },
+      post() {
+        return this.posts.find(el => el.slug === this.slug);
+      }
+    },
+    data() {
+      return {
+        slug: this.$route.params.slug
+      };
+    },
+    created() {
+      this.$store.dispatch("getPosts");
+    }
+    }
+    </script>
